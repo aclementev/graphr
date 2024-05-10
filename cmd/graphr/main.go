dba47a15f9b30c769fa273e41e9ea18170268f3c
Alvaro Clemente <clemente.varo@gmail.com>
2024-05-11 00:01:37 +0200

    feat: initial server setup

--- cmd/graphr/main.go
+++ cmd/graphr/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World!");
+	server := NewGraphrServer()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /hello/", server.helloHandler)
+
+	host := "127.0.0.1"
+	port := "8000"
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+
+	fmt.Println("Listening on", addr)
+	http.ListenAndServe(addr, mux)
+}
+
+type GraphrServer struct {
+}
+
+func NewGraphrServer() *GraphrServer {
+	return &GraphrServer{}
+}
+
+func (*GraphrServer) helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "got request %v", r.URL)
 }
